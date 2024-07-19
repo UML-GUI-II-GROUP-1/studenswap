9780dd51d79e38dbef4dba831dd4e39508c4b7fe
tejindra77 <tejindrakhatriofficial@gmail.com>
2024-07-19 19:35:36 -0400

    jwt secret

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
           PORT: 5000,
           NODE_ENV: 'development',
           MONGO_URI: 'mongodb://localhost:27017/studentswap',
-          JWT_SECRET: 'secret_key'
+          JWT_SECRET: 'f1c4c1c99bc9e6da1815c6bdb161e764c752a0f74beb29c1a161a41f5b1c44d1'
         },
         env_production: {
           NODE_ENV: 'production'
