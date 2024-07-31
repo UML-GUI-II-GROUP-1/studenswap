7994ad35e60ccdaa1f5fcfd82876de9c41aefc84
tejindra77 <tejindrakhatriofficial@gmail.com>
2024-07-31 09:35:08 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,23 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: true
-  },
-  lastname: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isVerified: { type: Boolean, default: false },
+  verificationToken: { type: String },
 });
 
 module.exports = mongoose.model('User', UserSchema);
