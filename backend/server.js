9dc2d0a5537f27cbd13999a49c659cec93efcd3a
tejindra77 <tejindrakhatriofficial@gmail.com>
2024-08-05 13:52:30 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const fs = require('fs');
 const morgan = require('morgan');
 
 dotenv.config();
@@ -11,16 +10,21 @@ dotenv.config();
 const app = express();
 
 // Connect Database
-connectDB().catch(err => console.error(err));
+connectDB();
 
 // Init Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan('combined', { stream: fs.createWriteStream('./access.log', { flags: 'a' }) }));
+app.use(morgan('dev'));
 
 // Define Routes
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/inventory', require('./routes/inventoryRoutes'));
+
+// Sample endpoint for testing
+app.get('/api/sample-endpoint', (req, res) => {
+  res.json({ message: 'Sample endpoint working!' });
+});
 
 // Root Route
 app.get('/', (req, res) => {
@@ -29,8 +33,3 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// Sample endpoint for testing
-app.get('/api/sample-endpoint', (req, res) => {
-  res.json({ message: 'Sample endpoint working!' });
-});
