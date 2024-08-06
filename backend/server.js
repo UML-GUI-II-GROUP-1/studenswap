3fa67f6e9c7d613a3998cc2c52711ada310253c6
tejindra77 <tejindrakhatriofficial@gmail.com>
2024-08-06 14:29:28 -0400

    port change

--- backend/server.js
+++ backend/server.js
@@ -31,5 +31,5 @@ app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
