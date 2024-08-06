cee30bec9d167ebe62048fd2ca68880d80f6eae1
tejindra77 <tejindrakhatriofficial@gmail.com>
2024-08-06 11:44:45 -0400

    fixes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,10 +1,18 @@
 const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
 
-module.exports = function (req, res, next) {
+dotenv.config();
+
+module.exports = (req, res, next) => {
+  // Get token from header
   const token = req.header('x-auth-token');
+
+  // Check if no token
   if (!token) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
+
+  // Verify token
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded.user;
