24df38d39d17704ddceedf11922908cd502ba6db
tejindra77 <tejindrakhatriofficial@gmail.com>
2024-08-06 14:39:25 -0400

    ecosystem update

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,18 +1,25 @@
 module.exports = {
-    apps: [
-      {
-        name: 'studentswap',
-        script: 'server.js',
-        env: {
-          PORT: 5000,
-          NODE_ENV: 'development',
-          MONGO_URI: 'mongodb://localhost:27017/studentswap',
-          JWT_SECRET: 'f1c4c1c99bc9e6da1815c6bdb161e764c752a0f74beb29c1a161a41f5b1c44d1'
-        },
-        env_production: {
-          NODE_ENV: 'production'
-        }
+  apps: [
+    {
+      name: 'studentswap-backend',
+      script: 'server.js',
+      env: {
+        PORT: 5000,
+        NODE_ENV: 'development',
+        MONGO_URI: 'mongodb://localhost:27017/studentswap',
+        JWT_SECRET: 'f1c4c1c99bc9e6da1815c6bdb161e764c752a0f74beb29c1a161a41f5b1c44d1'
+      },
+      env_production: {
+        NODE_ENV: 'production'
       }
-    ]
-  };
-  
+    },
+    {
+      name: 'studentswap-frontend',
+      script: 'node_modules/.bin/live-server',
+      args: 'public',
+      env: {
+        PATH: '/usr/bin:/usr/local/bin:/bin'
+      }
+    }
+  ]
+};
